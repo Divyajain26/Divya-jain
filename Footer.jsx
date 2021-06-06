d4ef8d1897961e67ff19a1c3a151dd324df54194
Divyajain26 <85454402+Divyajain26@users.noreply.github.com>
2021-06-07 00:01:03 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer () {
+  var currYear  = new Date().getFullYe
+
+  return <footer>
+    <p>Copyright @ {currYear}</p>
+    </footer>;
+};
+
+
+export default Footer;
